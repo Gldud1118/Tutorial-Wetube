c2a4c5c2293a9f29e7b9563512ffb2a916eb231e
gldud <gl171111@gmail.com>
2019-02-21 16:45:01 +0900

    템플릿 레이아웃 분리 및 템플릿으로의 데이터 전달을 위한 미들웨어 추가

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
@@ -10,6 +11,13 @@ import videoRouter from "./routers/videoRouter";
 
 const app = express();
 app.set("view engine", "pug"); //템플릿 엔진으로 pug사용
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(localsMiddleware);
+app.use(helmet());
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
