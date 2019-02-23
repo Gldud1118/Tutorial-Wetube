f36a68f34bd7e56d62395b6d9098103b9cf7b683
gldud <gl171111@gmail.com>
2019-02-23 14:55:09 +0900

    Passport로 로그인, 회원가입 기능 구현 및 세션 관리, 저장을 위한 미들웨어와 세션 스토어 추가

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,9 +1,37 @@
+import passport from "passport";
+import routes from "../routes";
+import User from "../models/User";
+
 export const getJoin = (req, res) => res.render("join");
-export const postJoin = (req, res) => {
-  res.redirect("/");
+
+export const postJoin = async (req, res, next) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+
+  if (password !== password2) {
+    res.status(400);
+    res.redirect("join", { pagTitle: "Join" });
+  } else {
+    try {
+      const user = await User({
+        name,
+        email
+      });
+
+      await User.register(user, password);
+      next();
+    } catch (err) {
+      console.log(err);
+    }
+  }
 };
 export const getLogin = (req, res) => res.render("login");
-export const postLogin = (req, res) => res.render("login");
+
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home
+});
 export const logout = (req, res) => res.send("logout");
 
 export const userDetail = (req, res) => res.render("userDetail");
